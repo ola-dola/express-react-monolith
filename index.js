06bced4afdfa634a968a9a2a3529341c9d685215
ola-dola <oredolaolamide@gmail.com>
2021-07-06 03:20:34 +0100

    server wip: configure dotenv setup engines in package.json

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+require("dotenv").config();
 
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/api/hello", (req, res) => {
+  res.send("Hello!!");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
